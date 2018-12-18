0dda1a6598a9164248a1a196b92818b987907468
karthik <karthikr298@gmail.com>
2018-12-18 17:06:43 -0500

    Code to handle post request

---
+++ Ex_15_HandlingPostReq/app.js
@@ -0,0 +1,34 @@
+/*
+ * handling post requests 
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set('view engine', 'ejs');
+app.use('/assets',express.static('assets'));
+
+app.get('/',function(req,res){  
+    res.render('index');
+});
+
+app.get('/contact',function(req,res){
+    res.render('contact',{qs:req.query});
+});
+
+app.post('/contact', urlEncodedParser, function(req,res){
+	console.log(req.body);
+    res.render('contact-success',{data:req.body});
+});
+
+
+
+
+app.get('/profile/:name',function(req,res){
+   var data = { age: '29',job: 'Ninja', hobbies:[ 'Eating','sleeping','programming']}; 
+   res.render('profile',{ person: req.params.name, data : data});
+});
+
+app.listen(3000);
