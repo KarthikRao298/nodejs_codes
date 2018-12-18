e145edb7f8fe097aaf874d0575799601d015abb3
karthik <karthikr298@gmail.com>
2018-12-18 17:06:54 -0500

    Integrating persistance data storage

---
+++ Ex_17_IntegPersDataStor/public_student_ex5/models/student.js
@@ -0,0 +1,14 @@
+/* @nanajjar */
+
+var student = function(firstName,lastName,degree, program) {
+    var studentObj = {
+        firstName:firstName,
+        lastName:lastName,
+        degree:degree,
+        program:program};
+  
+        return studentObj;
+  
+  };
+
+  module.exports.student = student;
